de6eadbd8bccd55a38adec80398d5f4aaf24a053
Ricardo Santos <ricardo.santos.diaz@gmail.com>
2023-05-15 00:50:05 +0200

    feat(trending): change management of zscores for ranking

--- trending/src/strategy/scoring.py
+++ trending/src/strategy/scoring.py
@@ -8,7 +8,7 @@ class ZScore(StrategyInterface):
         for col in ['numberOfLikes', 'numberOfPlayers']:
             col_zscore = col + '_zscore'
             data[col_zscore] = (data[col] - data[col].mean())/data[col].std(ddof=0)
-        data['score'] = abs(data['numberOfLikes_zscore'] + data['numberOfPlayers_zscore'])
+        data['score'] = data['numberOfLikes_zscore'] + data['numberOfPlayers_zscore']
         data = data.sort_values(by=['score'], ascending=False)
         data = data.reset_index(drop=True)
         data['rank'] = data.index
